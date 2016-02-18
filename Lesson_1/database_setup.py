fafcd0f83bbe77b2eb0867677af211b241fd71d8
Wasay Syed <wasay@witinc.com>
2016-02-18 15:45:39 -0500

    lesson 3 api call and json implementation added.

--- Lesson_1/database_setup.py
+++ Lesson_1/database_setup.py
@@ -27,9 +27,18 @@ class MenuItem(Base):
     restaurant = relationship(Restaurant)
 
 
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 
 
 Base.metadata.create_all(engine)
-
-print "database is setup - Lesson 1"
